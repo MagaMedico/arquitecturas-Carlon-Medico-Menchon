a37ed81a0e7ee983a1565d8f7eff5a8bf39076fb
Magalí Menchón <magalimenchon@gmail.com>
2021-09-05 19:00:25 -0300

    Se agregan modificaciones en clase Main, uso de instancias y renombre de clase

--- JDBCIntegrador1/src/main/Main.java
+++ JDBCIntegrador1/src/main/Main.java
@@ -11,28 +11,28 @@ import org.apache.commons.csv.CSVParser;
 import dao.*;
 import factory.*;
 
-public class main {
+public class Main {
 
 	public static void main(String[] args) throws SQLException, FileNotFoundException, IOException {
 		DAOFactory mysqlFactory = DAOFactory.getDAOFactory(DAOFactory.MYSQL_JDBC);
 		
 		//Crear las tablas
-		/*
+		
 		ClienteDAO clienteDAO = mysqlFactory.getClienteDAO();
 		FacturaDAO facturaDAO = mysqlFactory.getFacturaDAO();
 		ProductoDAO productoDAO = mysqlFactory.getProductoDAO();
 		FacturaProductoDAO facturaProductoDAO = mysqlFactory.getFacturaProductoDAO();
-		*/
+		
 		//Insertar los datos
 		CSVParser parserProducto = CSVFormat.DEFAULT.withHeader().parse(new FileReader("productos.csv"));
 		CSVParser parserCliente = CSVFormat.DEFAULT.withHeader().parse(new FileReader("clientes.csv"));
 		CSVParser parserFactura = CSVFormat.DEFAULT.withHeader().parse(new FileReader("facturas.csv"));
 		CSVParser parserFacturaProducto = CSVFormat.DEFAULT.withHeader().parse(new FileReader("facturas-productos.csv"));
 		
-		//mysqlFactory.getClienteDAO().insertCSV(parserCliente);
-		mysqlFactory.getFacturaDAO().insertCSV(parserFactura);
-		mysqlFactory.getProductoDAO().insertCSV(parserProducto);
-		mysqlFactory.getFacturaProductoDAO().insertCSV(parserFacturaProducto);
+		clienteDAO.insertCSV(parserCliente);
+		facturaDAO.insertCSV(parserFactura);
+		productoDAO.insertCSV(parserProducto);
+		facturaProductoDAO.insertCSV(parserFacturaProducto);
 	}
 
 }
